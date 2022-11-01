8aac4095c376e9bf1a98e90e979c4ce281de0f88
Rhylionn <tbernard.p@protonmail.com>
2022-11-01 12:56:52 +0100

    added i18n and vuehead

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from "vue"
 import "./index.css"
 import App from "./App.vue"
 
-createApp(App).mount("#app")
+import messages from "@intlify/vite-plugin-vue-i18n/messages"
+import { createI18n } from "vue-i18n"
+
+const i18n = createI18n({
+  locale: import.meta.env.VITE_APP_I18N_LOCALE,
+  fallbackLocale: import.meta.env.VITE_APP_I18N_FALLBACK_LOCALE,
+  messages,
+})
+
+const app = createApp(App)
+app.use(i18n)
+app.mount("#app")
