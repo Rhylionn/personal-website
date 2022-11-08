da31313815dc2ba38ab3dc07bc5c5544fdfbe7b4
Rhylionn <tbernard.p@protonmail.com>
2022-11-08 11:29:39 +0100

    started securing api

--- server/index.js
+++ server/index.js
@@ -2,19 +2,32 @@ const express = require("express")
 const path = require("path")
 const cors = require("cors")
 const db = require("./core/database")
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+dotenv.config()
 
 const app = express()
 
 // Define static route
 app.use("/static", express.static(path.join(__dirname, "public")))
 
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+  })
+)
+
 // Enable cors
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CLIENT,
+  })
+)
 
 app.use("/educations", require("./routes/educations"))
 app.use("/projects", require("./routes/projects"))
 app.use("/experiences", require("./routes/experiences"))
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000")
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`)
 })
