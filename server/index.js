2fac3a7f0f6a7012712ff647e0a45a1ffbd148d3
Rhylionn <tbernard.p@protonmail.com>
2022-11-27 21:42:11 +0100

    added security layer

--- server/index.js
+++ server/index.js
@@ -1,33 +1,44 @@
-const express = require("express")
-const path = require("path")
-const cors = require("cors")
-const db = require("./core/database")
-const dotenv = require("dotenv")
-const helmet = require("helmet")
-dotenv.config()
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const db = require("./core/database");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const https = require("https");
 
-const app = express()
+const fs = require("fs");
+dotenv.config();
+
+const app = express();
 
 // Define static route
-app.use("/static", express.static(path.join(__dirname, "public")))
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use(
   helmet({
     crossOriginEmbedderPolicy: false,
   })
-)
+);
 
 // Enable cors
 app.use(
   cors({
-    origin: process.env.CLIENT,
+    origin: "*",
   })
-)
+);
 
-app.use("/educations", require("./routes/educations"))
-app.use("/projects", require("./routes/projects"))
-app.use("/experiences", require("./routes/experiences"))
+app.use("/educations", require("./routes/educations"));
+app.use("/projects", require("./routes/projects"));
+app.use("/experiences", require("./routes/experiences"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`)
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync("/etc/letsencrypt/live/thomasbd.site/privkey.pem"),
+      cert: fs.readFileSync("/etc/letsencrypt/live/thomasbd.site/cert.pem"),
+    },
+    app
+  )
+  .listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+  });
